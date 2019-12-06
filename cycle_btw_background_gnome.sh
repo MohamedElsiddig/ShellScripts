ae7eae98a78370779a19680856f4e508dff39b1b
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-12-06 13:28:43 +0200

    Updated cycle_btw_background_gnome.sh

--- cycle_btw_background_gnome.sh
+++ cycle_btw_background_gnome.sh
@@ -5,19 +5,19 @@
 #  done
 #done
 
-#while [[ 1 -eq 1 ]]; do
-#  #for i in $(echo /home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/*.jpg); do
-#  for i in $(echo /home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/Catalina/*.jpg); do
-#    gsettings set org.gnome.desktop.background picture-uri file:///"${i}"
-#    sleep 5;
-#  done
-#done
+while [[ 1 -eq 1 ]]; do
+ #for i in $(echo /home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/*.jpg); do
+ for i in $(echo /home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/winter/*.jpg); do
+   gsettings set org.gnome.desktop.background picture-uri file:///"${i}"
+   sleep 60;
+ done
+done
 
-h=`date +%H`
-if [[ $h -lt 12 && $h -lt 18 ]]; then
-  gsettings set org.gnome.desktop.background picture-uri file:///home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/Catalina/Wallpaper-Day.jpg
-elif [[ $h -ge 18 && $h -gt 12 ]]; then
-  gsettings set org.gnome.desktop.background picture-uri file:///home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/Catalina/Wallpaper-Night.jpg
-else
-:
-fi
+# h=`date +%H`
+# if [[ $h -lt 12 && $h -lt 18 ]]; then
+#   gsettings set org.gnome.desktop.background picture-uri file:///home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/Catalina/Wallpaper-Day.jpg
+# elif [[ $h -ge 18 && $h -gt 12 ]]; then
+#   gsettings set org.gnome.desktop.background picture-uri file:///home/mohamedelsiddig/Pictures/MyWallpaper/NewWallpapers/Catalina/Wallpaper-Night.jpg
+# else
+# :
+# fi
